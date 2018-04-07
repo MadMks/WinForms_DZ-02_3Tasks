bae2fd7d8b2bbfaaa80ef35c2879c103674386c3
MadMks <mks59k@gmail.com>
2018-04-07 17:38:21 +0300

    Working 1 task.

--- Task_1_DayOftheWeek/Form1.Designer.cs
+++ Task_1_DayOftheWeek/Form1.Designer.cs
@@ -28,12 +28,45 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.datePicker = new System.Windows.Forms.DateTimePicker();
+            this.labelDayOfWeek = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // datePicker
+            // 
+            this.datePicker.Location = new System.Drawing.Point(26, 29);
+            this.datePicker.Name = "datePicker";
+            this.datePicker.Size = new System.Drawing.Size(162, 21);
+            this.datePicker.TabIndex = 0;
+            this.datePicker.ValueChanged += new System.EventHandler(this.datePicker_ValueChanged);
+            // 
+            // labelDayOfWeek
+            // 
+            this.labelDayOfWeek.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.labelDayOfWeek.Location = new System.Drawing.Point(26, 75);
+            this.labelDayOfWeek.Name = "labelDayOfWeek";
+            this.labelDayOfWeek.Padding = new System.Windows.Forms.Padding(3);
+            this.labelDayOfWeek.Size = new System.Drawing.Size(162, 22);
+            this.labelDayOfWeek.TabIndex = 1;
+            this.labelDayOfWeek.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(214, 133);
+            this.Controls.Add(this.labelDayOfWeek);
+            this.Controls.Add(this.datePicker);
+            this.Name = "Form1";
+            this.Text = "День недели";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DateTimePicker datePicker;
+        private System.Windows.Forms.Label labelDayOfWeek;
     }
 }
 
--- Task_1_DayOftheWeek/Form1.cs
+++ Task_1_DayOftheWeek/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,6 +17,61 @@ namespace Task_1_DayOftheWeek
         {
             InitializeComponent();
 
+            this.Load += Form1_Load;
         }
+
+
+        /// <summary>
+        /// Обработчик события при загрузке формы.
+        /// </summary>
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.ShowDayInLabel();
+        }
+
+
+        /// <summary>
+        /// Обработчик события изменений в строке даты.
+        /// </summary>
+        private void datePicker_ValueChanged(object sender, EventArgs e)
+        {
+            this.ShowDayInLabel();
+        }
+
+
+        /// <summary>
+        /// Выводим день в Label.
+        /// </summary>
+        private void ShowDayInLabel()
+        {
+            this.labelDayOfWeek.Text
+                = this.ConvertDayIntoRussian(this.datePicker.Value.DayOfWeek);
+            
+            this.ChangeFirstLetterToUppercase();
+        }
+
+
+        /// <summary>
+        /// Меняем первую букву на заглавную.
+        /// </summary>
+        private void ChangeFirstLetterToUppercase()
+        {
+            this.labelDayOfWeek.Text
+                = this.labelDayOfWeek.Text.Substring(0, 1).ToUpper()
+                + this.labelDayOfWeek.Text.Substring(1);
+        }
+
+
+        /// <summary>
+        /// Конвертируем день на русский язык.
+        /// </summary>
+        /// <param name="dayOfWeek">День недели из dataTimePicker.</param>
+        /// <returns>день недели на русском языке.</returns>
+        private string ConvertDayIntoRussian(DayOfWeek dayOfWeek)
+        {
+            return CultureInfo.GetCultureInfo("ru-RU").DateTimeFormat.GetDayName(dayOfWeek);
+        }
+
+
     }
 }
