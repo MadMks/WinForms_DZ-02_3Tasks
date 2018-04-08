629a11bfc68e78180b826473964459f562578cde
MadMks <mks59k@gmail.com>
2018-04-08 14:50:05 +0300

    Working task 2.

--- Task_2_TimeIsLeft/Form1.Designer.cs
+++ Task_2_TimeIsLeft/Form1.Designer.cs
@@ -34,15 +34,21 @@
             this.groupBoxAnswer = new System.Windows.Forms.GroupBox();
             this.labelAnswer = new System.Windows.Forms.Label();
             this.buttonTimeCompute = new System.Windows.Forms.Button();
+            this.radioButtonYear = new System.Windows.Forms.RadioButton();
+            this.radioButtonMonth = new System.Windows.Forms.RadioButton();
+            this.radioButtonDay = new System.Windows.Forms.RadioButton();
+            this.radioButtonMinute = new System.Windows.Forms.RadioButton();
+            this.radioButtonSecond = new System.Windows.Forms.RadioButton();
+            this.groupBoxRadioButton.SuspendLayout();
             this.groupBoxAnswer.SuspendLayout();
             this.SuspendLayout();
             // 
             // maskedTextBoxDate
             // 
-            this.maskedTextBoxDate.Location = new System.Drawing.Point(12, 33);
+            this.maskedTextBoxDate.Location = new System.Drawing.Point(99, 11);
             this.maskedTextBoxDate.Mask = "00/00/0000";
             this.maskedTextBoxDate.Name = "maskedTextBoxDate";
-            this.maskedTextBoxDate.Size = new System.Drawing.Size(187, 21);
+            this.maskedTextBoxDate.Size = new System.Drawing.Size(66, 21);
             this.maskedTextBoxDate.TabIndex = 0;
             this.maskedTextBoxDate.ValidatingType = typeof(System.DateTime);
             this.maskedTextBoxDate.KeyDown += new System.Windows.Forms.KeyEventHandler(this.maskedTextBoxDate_KeyDown);
@@ -58,9 +64,14 @@
             // 
             // groupBoxRadioButton
             // 
-            this.groupBoxRadioButton.Location = new System.Drawing.Point(12, 70);
+            this.groupBoxRadioButton.Controls.Add(this.radioButtonSecond);
+            this.groupBoxRadioButton.Controls.Add(this.radioButtonMinute);
+            this.groupBoxRadioButton.Controls.Add(this.radioButtonDay);
+            this.groupBoxRadioButton.Controls.Add(this.radioButtonMonth);
+            this.groupBoxRadioButton.Controls.Add(this.radioButtonYear);
+            this.groupBoxRadioButton.Location = new System.Drawing.Point(11, 45);
             this.groupBoxRadioButton.Name = "groupBoxRadioButton";
-            this.groupBoxRadioButton.Size = new System.Drawing.Size(200, 100);
+            this.groupBoxRadioButton.Size = new System.Drawing.Size(222, 141);
             this.groupBoxRadioButton.TabIndex = 2;
             this.groupBoxRadioButton.TabStop = false;
             this.groupBoxRadioButton.Text = "Вариант вывода";
@@ -68,9 +79,9 @@
             // groupBoxAnswer
             // 
             this.groupBoxAnswer.Controls.Add(this.labelAnswer);
-            this.groupBoxAnswer.Location = new System.Drawing.Point(15, 202);
+            this.groupBoxAnswer.Location = new System.Drawing.Point(13, 265);
             this.groupBoxAnswer.Name = "groupBoxAnswer";
-            this.groupBoxAnswer.Size = new System.Drawing.Size(252, 50);
+            this.groupBoxAnswer.Size = new System.Drawing.Size(220, 50);
             this.groupBoxAnswer.TabIndex = 3;
             this.groupBoxAnswer.TabStop = false;
             this.groupBoxAnswer.Text = "До указанной даты осталось:";
@@ -86,26 +97,81 @@
             // 
             // buttonTimeCompute
             // 
-            this.buttonTimeCompute.Location = new System.Drawing.Point(118, 177);
+            this.buttonTimeCompute.Location = new System.Drawing.Point(74, 206);
             this.buttonTimeCompute.Name = "buttonTimeCompute";
-            this.buttonTimeCompute.Size = new System.Drawing.Size(75, 23);
+            this.buttonTimeCompute.Size = new System.Drawing.Size(96, 39);
             this.buttonTimeCompute.TabIndex = 4;
             this.buttonTimeCompute.Text = "Рассчитать";
             this.buttonTimeCompute.UseVisualStyleBackColor = true;
             this.buttonTimeCompute.Click += new System.EventHandler(this.buttonTimeCompute_Click);
             // 
+            // radioButtonYear
+            // 
+            this.radioButtonYear.AutoSize = true;
+            this.radioButtonYear.Location = new System.Drawing.Point(7, 20);
+            this.radioButtonYear.Name = "radioButtonYear";
+            this.radioButtonYear.Size = new System.Drawing.Size(64, 17);
+            this.radioButtonYear.TabIndex = 0;
+            this.radioButtonYear.Text = "В годах";
+            this.radioButtonYear.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonMonth
+            // 
+            this.radioButtonMonth.AutoSize = true;
+            this.radioButtonMonth.Location = new System.Drawing.Point(7, 43);
+            this.radioButtonMonth.Name = "radioButtonMonth";
+            this.radioButtonMonth.Size = new System.Drawing.Size(75, 17);
+            this.radioButtonMonth.TabIndex = 1;
+            this.radioButtonMonth.Text = "В месяцах";
+            this.radioButtonMonth.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonDay
+            // 
+            this.radioButtonDay.AutoSize = true;
+            this.radioButtonDay.Checked = true;
+            this.radioButtonDay.Location = new System.Drawing.Point(7, 66);
+            this.radioButtonDay.Name = "radioButtonDay";
+            this.radioButtonDay.Size = new System.Drawing.Size(59, 17);
+            this.radioButtonDay.TabIndex = 2;
+            this.radioButtonDay.TabStop = true;
+            this.radioButtonDay.Text = "В днях";
+            this.radioButtonDay.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonMinute
+            // 
+            this.radioButtonMinute.AutoSize = true;
+            this.radioButtonMinute.Location = new System.Drawing.Point(7, 89);
+            this.radioButtonMinute.Name = "radioButtonMinute";
+            this.radioButtonMinute.Size = new System.Drawing.Size(76, 17);
+            this.radioButtonMinute.TabIndex = 3;
+            this.radioButtonMinute.Text = "В минутах";
+            this.radioButtonMinute.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonSecond
+            // 
+            this.radioButtonSecond.AutoSize = true;
+            this.radioButtonSecond.Location = new System.Drawing.Point(7, 112);
+            this.radioButtonSecond.Name = "radioButtonSecond";
+            this.radioButtonSecond.Size = new System.Drawing.Size(82, 17);
+            this.radioButtonSecond.TabIndex = 4;
+            this.radioButtonSecond.Text = "В секундах";
+            this.radioButtonSecond.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(286, 264);
+            this.ClientSize = new System.Drawing.Size(245, 332);
             this.Controls.Add(this.buttonTimeCompute);
             this.Controls.Add(this.groupBoxAnswer);
             this.Controls.Add(this.groupBoxRadioButton);
             this.Controls.Add(this.labelTextBoxDate);
             this.Controls.Add(this.maskedTextBoxDate);
+            this.MaximizeBox = false;
             this.Name = "Form1";
             this.Text = "Остаток времени";
+            this.groupBoxRadioButton.ResumeLayout(false);
+            this.groupBoxRadioButton.PerformLayout();
             this.groupBoxAnswer.ResumeLayout(false);
             this.groupBoxAnswer.PerformLayout();
             this.ResumeLayout(false);
@@ -121,6 +187,11 @@
         private System.Windows.Forms.GroupBox groupBoxAnswer;
         private System.Windows.Forms.Button buttonTimeCompute;
         private System.Windows.Forms.Label labelAnswer;
+        private System.Windows.Forms.RadioButton radioButtonYear;
+        private System.Windows.Forms.RadioButton radioButtonSecond;
+        private System.Windows.Forms.RadioButton radioButtonMinute;
+        private System.Windows.Forms.RadioButton radioButtonDay;
+        private System.Windows.Forms.RadioButton radioButtonMonth;
     }
 }
 
--- Task_2_TimeIsLeft/Form1.cs
+++ Task_2_TimeIsLeft/Form1.cs
@@ -12,7 +12,16 @@ namespace Task_2_TimeIsLeft
 {
     public partial class Form1 : Form
     {
-        DateTime enteredDate;
+        private const int NUMBER_OF_DAYS_IN_A_YEAR = 365;
+        private const int NUMBER_OF_DAYS_IN_A_MONTH = 30;
+        private const int NUMBER_OF_MINUTES_IN_A_DAY = 1440;
+        private const int NUMBER_OF_MINUTES_IN_A_HOURS = 60;
+        private const int NUMBER_OF_SECONDS_IN_A_MINUTE = 60;
+
+
+        DateTime enteredDate;   // TODO
+        TimeSpan _timeLeft;
+        string _answerStr;
 
         public Form1()
         {
@@ -24,6 +33,7 @@ namespace Task_2_TimeIsLeft
         {
             if (e.KeyData == Keys.Enter)
             {
+                // то событие Ентер кнопки
                 this.CheckDate();
             }
         }
@@ -41,21 +51,109 @@ namespace Task_2_TimeIsLeft
                 // вывести в Ответ что "введенная дата уже была"
                 if (this.IsTheDateWasAlready() == true)
                 {
-                    this.labelAnswer.Text = "Дата уже была";
+                    //this.labelAnswer.Text = "Дата уже была";
+                    this.DisplayTheAnswer("Дата уже была");
                 }
                 else
                 {
+                    // вычисления оставшегося времени
+                    this.ComputeTheRemainingTime();
+
                     // вывести ответ в лабел
-                    this.DisplayTheAnswer();
+                    this.DisplayTheAnswer(_answerStr);
                 }
             }
             else
             {
                 // подсветить красным дату
+                //this.maskedTextBoxDate.BackColor = Color.FromArgb(232, 107, 107);   // TODO
                 // или показать текст что неправильная дата
-                this.labelAnswer.Text = "Дата не существует";
+                //this.labelAnswer.Text = "Дата не существует";
+                this.DisplayTheAnswer("Дата не существует");
             }
         }
+
+        private void ComputeTheRemainingTime()
+        {
+            // Вычисление разницы времени.
+            this._timeLeft = this.enteredDate - DateTime.Now;
+
+            // Сохранение и вычисление ответа
+            // соответственно выбора в radioButton.
+            if (this.radioButtonYear.Checked == true)
+            {
+                _answerStr = this.ComputeNumberOfYears();
+            }
+            else if (this.radioButtonMonth.Checked == true)
+            {
+                _answerStr = this.ComputeNumberOfMonths();
+            }
+            else if (this.radioButtonDay.Checked == true)
+            {
+                _answerStr = this.ComputeNumberOfDays();
+            }
+            else if (this.radioButtonMinute.Checked == true)
+            {
+                _answerStr = this.ComputeNumberOfMinutes();
+            }
+            else if (this.radioButtonSecond.Checked == true)
+            {
+                _answerStr = this.ComputeNumberOfSeconds();
+            }
+        }
+
+        private string ComputeNumberOfYears()
+        {
+            return Math.Round(
+                        (double)_timeLeft.Days / NUMBER_OF_DAYS_IN_A_YEAR, 1
+                        ).ToString()
+                    + " лет/год(а)";
+        }
+
+        private string ComputeNumberOfMonths()
+        {
+            return Math.Round(
+                        (double)_timeLeft.Days / NUMBER_OF_DAYS_IN_A_MONTH, 1
+                        ).ToString()
+                    + " месяц(ев)";
+        }
+
+        private string ComputeNumberOfDays()
+        {
+            return _timeLeft.Days.ToString()
+                    + " день/дней";
+        }
+
+        private string ComputeNumberOfMinutes()
+        {
+            return (
+                        (_timeLeft.Days * NUMBER_OF_MINUTES_IN_A_DAY)
+                        + (_timeLeft.Hours * NUMBER_OF_MINUTES_IN_A_HOURS)
+                        + _timeLeft.Minutes
+                    ).ToString()
+                    + " минут(а)";
+        }
+
+        /// <summary>
+        /// Вычислить количество секунд до указанной даты.
+        /// </summary>
+        /// <returns>Строка в которой указано "кол-во секунд".</returns>
+        private string ComputeNumberOfSeconds()
+        {
+            return (
+                        (
+                            (
+                                (_timeLeft.Days * NUMBER_OF_MINUTES_IN_A_DAY)
+                                + (_timeLeft.Hours * NUMBER_OF_MINUTES_IN_A_HOURS)
+                                + _timeLeft.Minutes
+                            )
+                            * NUMBER_OF_SECONDS_IN_A_MINUTE
+                        )
+                        + _timeLeft.Seconds
+                    ).ToString()
+                    + " секунд(а)";
+        }
+
 
 
         /// <summary>
@@ -81,29 +179,22 @@ namespace Task_2_TimeIsLeft
         /// <returns>true если дата корректна.</returns>
         private bool IsDateIsCorrect()
         {
-            
-            
-            //DateTime.TryParse(this.maskedTextBoxDate.Text, out dt);
-
             if (DateTime.TryParse(this.maskedTextBoxDate.Text, out enteredDate))
             {
-                //this.labelAnswer.Text = "true";
                 return true;
             }
             else
             {
-                //this.labelAnswer.Text = "false";
                 return false;
             }
         }
 
-        private void DisplayTheAnswer()
-        {
-            this.labelAnswer.Text = "test Click";
 
-            // проверить дату()
+        private void DisplayTheAnswer(string str)
+        {
+            this.labelAnswer.Text = str;
         }
 
-        
+
     }
 }
